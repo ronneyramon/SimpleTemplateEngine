0e75dffa3d0b6b2a2d40dfafa7e934524f30cc96
Ronney Ramon <ronney@ymail.com>
2020-05-18 17:44:57 -0300

    Add project files.

---
+++ SimpleTemplateEngine/StringTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleTemplateEngine
+{
+    public class StringTemplate
+    {
+        private string _template;
+
+        public StringTemplate(string template)
+        {
+            _template = template;
+        }
+
+        public string Apply(object model)
+        {
+            return _template;
+        }
+    }
+}
